55dc2b649095b04226df673da74386da718cef0c
Elliot Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2023-08-04 11:48:28 +0200

    feat: switch to spline for footer 3d rendering

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,92 +3,11 @@
 import {
   Avatar, Stack, Typography,
 } from '@mui/joy';
-import { AsciiRenderer } from '@react-three/drei';
-import {
-  Canvas, useFrame, useThree,
-} from '@react-three/fiber';
 import { ATypography } from 'App';
 import { HiOutlineCheckBadge } from 'react-icons/hi2';
-import React, { useEffect, useRef } from 'react';
-
-import {
-  Group, Mesh, MeshStandardMaterial, SphereGeometry,
-} from 'three';
+import React from 'react';
+import Spline from '@splinetool/react-spline';
 import { Default } from './Responsive';
-
-function Icosahedron() {
-  const meshRef = useRef<Mesh>(null);
-  const viewport = useThree((state) => state.viewport);
-  useFrame((state, delta) => {
-    if (meshRef.current) {
-      meshRef.current.rotation.x += delta / 10;
-      meshRef.current.rotation.y += delta / 10;
-    }
-  });
-  return (
-    <mesh scale={Math.min(viewport.width, viewport.height) / 5} ref={meshRef}>
-      <icosahedronGeometry args={[1.2, 1]} />
-      <meshStandardMaterial color="grey" />
-    </mesh>
-  );
-}
-
-function AsteroidRing() {
-  const groupRef = useRef<Group>(null);
-  const viewport = useThree((state) => state.viewport);
-  const numAsteroids = 500;
-  const radius = (2.4 * Math.min(viewport.width, viewport.height)) / 5;
-  const asteroidRadius = 0.05;
-  const asteroidGeometry = new SphereGeometry(asteroidRadius, 1, 1);
-  const asteroidMaterial = new MeshStandardMaterial({ color: 'grey' });
-
-  useEffect(() => {
-    if (groupRef.current) {
-      groupRef.current.rotation.set((-7.5 * Math.PI) / 16, (0.2 * Math.PI) / 4, 0);
-      for (let i = 0; i < numAsteroids; i++) {
-        const asteroid = new Mesh(asteroidGeometry, asteroidMaterial);
-        const angle = (i / numAsteroids) * Math.PI * 2;
-        const x = Math.cos(angle) * (radius + (Math.random() - 0.5));
-        const y = Math.sin(angle) * (radius + (Math.random() - 0.5));
-        const z = (Math.random() - 0.5) * 0.1;
-        asteroid.position.set(x, y, z);
-        asteroid.rotation.set(
-          Math.random() * Math.PI,
-          Math.random() * Math.PI,
-          Math.random() * Math.PI,
-        );
-        groupRef.current.add(asteroid);
-      }
-    }
-  }, [groupRef, numAsteroids, radius]);
-
-  useFrame((state, delta) => {
-    if (groupRef.current) {
-      groupRef.current.rotation.z += delta / 9;
-    }
-  });
-
-  return (
-    <group ref={groupRef} />
-  );
-}
-
-function View() {
-  return (
-    <Canvas style={{
-      height: '100%',
-    }}
-    >
-      <color attach="background" args={['black']} />
-      <pointLight position={[0, 0, 0]} />
-      <pointLight position={[-10, 0, 0]} />
-      <ambientLight intensity={0.05} />
-      <Icosahedron />
-      <AsteroidRing />
-      <AsciiRenderer fgColor="#ffe0e0" characters=" .,:-+*=/%@#" bgColor="transparent" />
-    </Canvas>
-  );
-}
 
 export default function Footer() {
   return (
@@ -98,7 +17,7 @@ export default function Footer() {
       sx={{ position: 'relative' }}
     >
       <Default>
-        <Stack sx={{ width: '100%', height: '300px' }}>
+        <Stack sx={{ width: '100%', height: '400px' }}>
           <Stack
             sx={{
               position: 'absolute',
@@ -145,7 +64,13 @@ export default function Footer() {
               Let&apos;s build products that people love. Together.
             </Typography>
           </Stack>
-          <View />
+          <Spline
+            scene="https://prod.spline.design/D7xRCZ6d7FKAmDLz/scene.splinecode"
+            style={{
+              width: '100%',
+              marginBottom: '-200px',
+            }}
+          />
         </Stack>
       </Default>
       <Stack
