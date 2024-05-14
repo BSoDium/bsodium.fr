ae570b74a7ce7252f2ec86695dda31a3109d264a
Elliot Négrel-Jerzy <negreljerzy.elliot@gmail.com>
2024-05-14 20:50:43 +0200

    refactor: update useLandScapeMode to include minAspectRatio in Responsive component

--- src/components/Responsive.tsx
+++ src/components/Responsive.tsx
@@ -9,7 +9,7 @@ export const useTabletMode = () => useMediaQuery({ minWidth: 768, maxWidth: 991 
 export const useMobileMode = () => useMediaQuery({ maxWidth: 767 });
 export const useNonMobileMode = () => useMediaQuery({ minWidth: 768 });
 export const useNonDesktopMode = () => useMediaQuery({ maxWidth: 991 });
-export const useLandScapeMode = () => useMediaQuery({ minWidth: 'calc(75rem + 10rem)' });
+export const useLandScapeMode = () => useMediaQuery({ minWidth: 'calc(75rem + 10rem)', minAspectRatio: '1/1' });
 
 export type ResponsiveComponent = (props: ResponsiveProps) => JSX.Element | null;
 
