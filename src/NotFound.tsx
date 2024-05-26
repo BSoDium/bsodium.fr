d0c4330a5b944b0b325534f17746919fde55e3f7
Elliot Négrel-Jerzy <negreljerzy.elliot@gmail.com>
2024-05-26 23:42:31 +0200

    refactor: Add NotFound component for handling 404 errors

---
+++ src/NotFound.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+}
