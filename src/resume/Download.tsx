b466b4f86af682f050640c28506ea0c438e29b4e
Elliot Négrel-Jerzy <negreljerzy.elliot@gmail.com>
2024-05-31 21:05:06 +0200

    refactor: improve download banner responsiveness on resume page

--- src/resume/Download.tsx
+++ src/resume/Download.tsx
@@ -1,14 +1,30 @@
 import {
-  Button, Card, Stack, Typography,
+  Button, Card, IconButton, Stack, Tooltip, Typography,
 } from '@mui/joy';
 import details from 'assets/Details';
-import React, { useState } from 'react';
+import { useMobileMode } from 'components/Responsive';
+import React, { createRef } from 'react';
+import { FaRegFilePdf } from 'react-icons/fa';
+import { FiDownload, FiPrinter } from 'react-icons/fi';
 
 export default function Download() {
-  const [fileName] = useState(`Resume_${details.name.first}_${details.name.last}.pdf`);
+  const mobile = useMobileMode();
+
+  const container = createRef<HTMLDivElement>();
+
+  const fileName = `Resume_${details.name.first}_${details.name.last}.pdf`;
+
+  const print = () => {
+    // noop
+  };
+
+  const download = () => {
+    // noop
+  };
+
   return (
     <Stack
-      id="container"
+      ref={container}
       direction="row"
       justifyContent="center"
       alignItems="center"
@@ -17,28 +33,90 @@ export default function Download() {
         bottom: 'var(--nav-safe-area-inset-bottom, 0)',
         width: '100%',
         paddingLeft: 'var(--nav-safe-area-inset-left, 0)',
-        borderRadius: 0,
         zIndex: 2,
       }}
     >
       <Card
-        variant="outlined"
+        variant={mobile ? 'plain' : 'outlined'}
         sx={(theme) => ({
-          width: 'min(100%, 30em)',
-          backgroundColor: `color-mix(in srgb, ${theme.palette.background.body}, transparent 50%)`,
+          borderRadius: mobile ? 0 : undefined,
+          width: mobile ? '100%' : '30rem',
+          marginBottom: mobile ? 0 : '1rem',
+          backgroundColor: `color-mix(in srgb, ${theme.palette.background.body}, transparent 30%)`,
           backdropFilter: 'blur(10px)',
           webkitBackdropFilter: 'blur(10px)',
           padding: '.5rem',
-          marginY: '.5rem',
+          boxShadow: 'lg',
         })}
       >
-        <Stack direction="row" alignItems="center" justifyContent="space-between">
-          <Typography>
-            {fileName}
+        <Stack direction="row" gap={1} alignItems="center" justifyContent="space-between">
+
+          <Typography
+            level="body2"
+            fontWeight={500}
+            marginLeft={0.7}
+            startDecorator={(
+              <FaRegFilePdf size="1.2rem" />
+          )}
+            sx={{
+              gap: 0.5,
+              flex: '1 1 100%',
+              minWidth: 0,
+            }}
+          >
+            <span style={{
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              whiteSpace: 'nowrap',
+              maxWidth: '100%',
+            }}
+            >
+              {fileName}
+            </span>
           </Typography>
-          <Button>
-            Download
-          </Button>
+          <Stack direction="row" gap={1}>
+            {mobile ? (
+              <Tooltip placement="top" variant="outlined" title="Print">
+                <IconButton
+                  color="neutral"
+                  variant="plain"
+                  onClick={print}
+                >
+                  <FiPrinter />
+                </IconButton>
+              </Tooltip>
+            ) : (
+              <Button
+                color="neutral"
+                variant="plain"
+                onClick={print}
+                startDecorator={(
+                  <FiPrinter />
+          )}
+              >
+                Print
+              </Button>
+            )}
+            {mobile ? (
+              <Tooltip placement="top" variant="outlined" title="Download">
+                <IconButton
+                  variant="solid"
+                  onClick={download}
+                >
+                  <FiDownload />
+                </IconButton>
+              </Tooltip>
+            ) : (
+              <Button
+                onClick={download}
+                startDecorator={(
+                  <FiDownload />
+          )}
+              >
+                Download
+              </Button>
+            )}
+          </Stack>
         </Stack>
       </Card>
     </Stack>
